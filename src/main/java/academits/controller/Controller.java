a0e87a41bf961e99cf661f52166a4691de82a7ca
aas86 <aas86@mail.ru>
2018-11-06 16:34:41 +0700

    Убрал закомментированный код

--- src/main/java/academits/controller/Controller.java
+++ src/main/java/academits/controller/Controller.java
@@ -1,122 +1,69 @@
 package academits.controller;
 
+import com.vk.api.sdk.client.TransportClient;
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.UserActor;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ApiWallLinksForbiddenException;
+import com.vk.api.sdk.exceptions.ClientException;
+import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.UserAuthResponse;
+import com.vk.api.sdk.objects.wall.responses.GetResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/testTask/api")
 public class Controller {
+    private static final Integer APP_ID = 6731226;
+    private static final String CLIENT_SECRET = "hLu7TXcJ7PKQ3vwvWoNI";
+    private static final String REDIRECT_URI = "http://localhost:8080/testTask/api/logIn";
+    private boolean needAddPost = false;
 
     @RequestMapping(value = "/logIn", method = RequestMethod.GET)
-    public String logIn(/*@RequestParam(value = "x", required = false) String x,*/
-                      @RequestParam(value = "code", required = false) String code,
-                      @RequestParam(value = "access_token", required = false) String accessToken,
-                      HttpServletResponse resp, HttpServletRequest req) throws IOException {
-        // System.out.println(x);
-        System.out.println(code);
-        System.out.println(accessToken);
-
-
-        if (code == null && accessToken == null) {
-            //resp.sendRedirect("https://oauth.vk.com/authorize?client_id=6731226&display=popup&redirect_uri=http://localhost:8080/testTask/api/logIn&scope=wall&response_type=code&v=5.87");
-            // resp.sendRedirect("http://ya.ru");
-            return "https://oauth.vk.com/authorize?client_id=6731226&display=popup&redirect_uri=http://localhost:8080/testTask/api/logIn&scope=wall&response_type=code&v=5.87";
-        } else if (code != null) {
-            String url = "https://oauth.vk.com/access_token?client_id=6731226&client_secret=hLu7TXcJ7PKQ3vwvWoNI&redirect_uri=http://localhost:8080/testTask/api/logIn&code=" + code;
-            //System.out.println(url);
-            return url;
+    public List logIn(@RequestParam(value = "code", required = false) String code,
+                      @RequestParam(value = "post", required = false) String post,
+                      /*@RequestParam(value = "access_token", required = false) String token,*/
+                      HttpServletResponse resp, HttpServletRequest request) throws IOException, ClientException, ApiException {
+        TransportClient transportClient = HttpTransportClient.getInstance();
+        VkApiClient vk = new VkApiClient(transportClient);
+        //System.out.println(code);
+        if (post == null && code == null) {
+            resp.setStatus(403);
+        } else if (post != null) {
+            needAddPost = true;
+            resp.setStatus(403);
         } else {
-             return accessToken;
-        }
-    }
-    //return "A";
+            UserAuthResponse authResponse = vk.oauth()
+                    .userAuthorizationCodeFlow(APP_ID, CLIENT_SECRET, REDIRECT_URI, code)
+                    .execute();
+            UserActor actor = new UserActor(authResponse.getUserId(), authResponse.getAccessToken());
+            if (!needAddPost) {
+                GetResponse getResponse = vk.wall().get(actor).execute();
+                getResponse.getItems();
+                return getResponse.getItems(); // Можно попробовать что-то другое вернуть
+            } else {
 
-/*
-    @RequestMapping(value = "/getToken", method = RequestMethod.GET)
-    public void getToken(@RequestParam(value = "code", required = true) String code,
-                         @RequestParam(value = "token", required = false) String token) {
-        System.out.println(code);
-        System.out.println(token);
-        StringBuilder sb = new StringBuilder();
-        sb.append("https://oauth.vk.com/access_token?client_id=6731226&client_secret=hLu7TXcJ7PKQ3vwvWoNI&redirect_uri=http://localhost:8080/testTask/api/getToken&code=")
-                .append(code);
-        System.out.println(sb.toString());
-        HttpURLConnection connection = null;
-        /*try {
-            connection = (HttpURLConnection) new URL(sb.toString()).openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
-            System.out.println(connection.getResponseMessage());
-            System.out.println(connection.getContent());
-            System.out.println(connection.getOutputStream().toString());
-            //return sb.toString();
-        } catch (Throwable e) {
-            e.printStackTrace();
-        } *//*finally {
-            if (connection != null) {
-                connection.disconnect();
-            }*/
-
-
-   /* @RequestMapping(value = "/signIn", method = RequestMethod.GET)
-  /*  public void logIn(@RequestParam(value = "login", required = true) String login,
-                        @RequestParam(value = "password", required = true) String password, HttpServletResponse resp) {
-        System.out.println("login = " + login);
-        System.out.println("password = " + password);
-
-        String url = "https://oauth.vk.com/authorize?client_id=6731226&display=page&redirect_uri=" +
-                "https://oauth.vk.com/blank.html/callback&scope=wall&response_type=token&v=5.87";
-
-
-       // UserActor actor = new UserActor(authResponse.getUserId(), authResponse.getAccessToken());
-        HttpURLConnection connection = null;
-        try {
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
-            System.out.println(connection.getResponseMessage());
-            StringBuilder sb = new StringBuilder();
-            if (HttpURLConnection.HTTP_OK == connection.getResponseCode()) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    sb.append(line);
-                    sb.append("\n");
+                try {
+                    needAddPost = false;
+                    vk.wall().post(actor).message("Hello world").execute();
+                } catch (ApiWallLinksForbiddenException e) {
+                    // Links posting is prohibited
+                } catch (ApiException e) {
+                    // Business logic error
+                } catch (ClientException e) {
+                    // Transport layer error
                 }
-                 System.out.println(sb.toString());
             }
-        } catch (Throwable e) {
-            e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-
         }
-        //resp.setStatus(300);
-        //return url;
-
-
-
-    }*/
-
-
+        return null;
+    }
 }
-
--- src/main/resources/static/js/vk.js
+++ src/main/resources/static/js/vk.js
@@ -3,31 +3,54 @@
  */
 $(document).ready(function () {
 
+    var first_btn = $("#get_all");
+    var second_btn = $("#add_post");
+    first_btn.on('click', function () {
 
-    var btn = $("#begin");
-    btn.on('click', function () {
         $.ajax({
             type: "GET",
             url: "http://localhost:8080//testTask/api/logIn",
             dataType: 'json',
             contentType: "application/json; charset=utf-8"
 
-        }).always(function (response) {
-            console.log(response);
-            var x = response.responseText;
-            console.log(x);
+        }).fail(function (response) {
+            window.location.href = "https://oauth.vk.com/authorize?client_id=6731226&display=page&redirect_uri=http://localhost:8080/testTask/api/logIn&scope=wall&response_type=code&v=5.87";
 
-        });
-           // data: JSON.stringify(x)
-        });/*.done(function (response) {
-            console.log(response);
-            //console.log(JSON.parse(response));
-            //console.log("Добавилось. Всё ОК");
-            //console.log(response);
+        })/*.always(function (response) {
+         console.log(response);
+         var x = response.responseText;
+         console.log(x);
+
+         })*/;
+        // data: JSON.stringify(x)
+    });
+
+    second_btn.on('click', function () {
+        var x = {
+            post: "true"
+        };
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:8080/testTask/api/logIn",
+            dataType: 'json',
+            contentType: "application/json; charset=utf-8",
+            data: x
+        }).done(function (response) {
 
         }).fail(function (response) {
-            console.log(response);
-        })*/
+            window.location.href = "https://oauth.vk.com/authorize?client_id=6731226&display=popup&redirect_uri=http://localhost:8080/testTask/api/logIn&scope=wall&response_type=code&v=5.87";
+
+        })
+    });
+    /*.done(function (response) {
+     console.log(response);
+     //console.log(JSON.parse(response));
+     //console.log("Добавилось. Всё ОК");
+     //console.log(response);
+
+     }).fail(function (response) {
+     console.log(response);
+     })*/
 
 
     /*btn.on('click', function () {
